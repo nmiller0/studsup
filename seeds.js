21b20584f019a19529dd43af8781c1980d876108
Neal Miller <neal.miller@outlook.com>
2019-01-28 10:52:39 -0500

    Kinda got the seeds file working

--- seeds.js
+++ seeds.js
@@ -3,69 +3,93 @@ var Player = require("./models/player");
 var Team = require("./models/team");
 var faker = require("faker");
 
+
 var teamOne = {name: "Sporting Enkelman", city: "Enk City"};
 var teamTwo = {name: "Enkelman FC", city: "Enkopolis"};
 
 var postions = ["GK", "DEF", "MID", "ATT"]
 var playersOne = [];
-for(var i = 0; i < 11; i++){
+for(var i = 0; i < 15; i++){
     var p = {};
     p.name = faker.name.findName()
     p.age = 22;
-    p.att = Math.random * 5
-    p.def = Math.random * 5
-    p.mid = Math.random * 5
-    p.position = postions[(Math.ceil( Math.random * 3))];
-    playersTwo.push(p);
+    p.att = Math.random() * 5;
+    p.def = Math.random() * 5;
+    p.mid = Math.random() * 5;
+    p.position = postions[(Math.ceil( Math.random() * 3))];
+    playersOne.push(p);
 }
 
 var playersTwo = [];
-for(var i = 0; i < 11; i++){
+for(var i = 0; i < 15; i++){
     var p = {};
     p.name = faker.name.findName()
     p.age = 22;
-    p.att = Math.random * 5
-    p.def = Math.random * 5
-    p.mid = Math.random * 5
-    p.position = postions[(Math.ceil( Math.random * 3))];
+    p.att = Math.random() * 5
+    p.def = Math.random() * 5
+    p.mid = Math.random() * 5
+    p.position = postions[(Math.ceil( Math.random() * 3))];
     playersTwo.push(p);
 }
-Team.create(teamOne, function(err, newTeam){
-    if(err){
-        console.log(err);
-    } else{
-        console.log(newTeam);
-        playersOne.forEach(function(player){
-            Player.create(player, function(err, newPlayer){ 
-                if(err){
-                    console.log(err);
-                } else{
-                    console.log(newPlayer);
-                    newTeam.players.push(newPlayer);
-                    newTeam.save();
-                }
-            })
-        });
-    }
-});
-Team.create(teamTwo, function(err, newTeam){
-    if(err){
-        console.log(err);
-    } else{
-        console.log(newTeam);
-        playersTwo.forEach(function(player){
-            Player.create(player, function(err, newPlayer){ 
-                if(err){
-                    console.log(err);
-                } else{
-                    console.log(newPlayer);
-                    newTeam.players.push(newPlayer);
-                    newTeam.save();
-                }
-            })
-        });
-    }
+var players2Mongoose = [];
+playersTwo.forEach(function(player){
+    Player.create(player, function(err, newPlayer){ 
+        if(err){
+            console.log(err);
+        } else{
+            //console.log(newPlayer);
+            players2Mongoose.push(newPlayer);
+        }
+    });
 });
 
+var players1Mongoose = [];
+playersOne.forEach(function(player){
+    Player.create(player, function(err, newPlayer){ 
+        if(err){
+            console.log(err);
+        } else{
+            //console.log(newPlayer);
+            players1Mongoose.push(newPlayer);
+        }
+    });
+});
+// is the problem just that these functions depend on one another?
+function seedDB(){
+    teamOne.players = players1Mongoose;
+    teamTwo.players = players2Mongoose;
+    Team.create(teamOne, function(err, newTeam){
+        if(err){
+            console.log(err);
+        } else{
+            console.log(newTeam);
+            Player.find({}, function(err, players){
+                var onePlayers = players.slice(0,12);
+                console.log(onePlayers.length);
+                onePlayers.forEach(function(player){
+                    newTeam.players.push(player);
+                });
+                newTeam.save();
+            });
+        }
+    });
+    Team.create(teamTwo, function(err, newTeam){
+        if(err){
+            console.log(err);
+        } else{
+            console.log(newTeam);
+            Player.find({}, function(err, players){
+                var onePlayers = players.slice(12,25);
+                console.log(onePlayers.length);
+                onePlayers.forEach(function(player){
+                    newTeam.players.push(player);
+                });
+                newTeam.save();
+            });
+        }
+    });
+};
+
+module.exports = seedDB;
 
 
