4bece7d936fc9b6b0d282ff62585591357c327c0
Neal Miller <neal.miller@outlook.com>
2019-01-30 12:28:56 -0500

    Added contents to indexes

---
+++ models/index.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+module.exports.Match= require("./match");
+module.exports.League = require("./league");
+module.exports.Player = require("./player");
+module.exports.Team = require("./team")
+
