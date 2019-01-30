a20b7ce4a34a1fb9c3f67de26a8f64f4229547f9
Neal Miller <neal.miller@outlook.com>
2019-01-30 12:28:43 -0500

    Wiggled match.js around

--- controllers/match.js
+++ controllers/match.js
@@ -1,25 +1,30 @@
 var Match = require("../models/match");
+var Player = require("../models/")
 var models = require("../models")
 var matchController = {};
 
 module.exports.playMatch = function (homeTeam, awayTeam) {
-    console.log(homeTeam);
     var homeDef = 0;
     var homeAtt = 0;
     var homeMid = 0;
     homeTeam.players.forEach(function(player){
-       homeAtt += player.att;
-       homeMid += player.mid;
-       homeDef += player.def;
+        // Problem is that the program marches on and does not include this info in the coeff
+        models.Player.findById(player, function(err, foundPlayer){
+            homeAtt += foundPlayer.att;
+            homeMid += foundPlayer.mid;
+            homeDef += foundPlayer.def;
+        });
     });
     var homeCoeff = homeDef + homeAtt + homeMid + 1;
+    console.log(homeCoeff);
     var awayDef = 0;
     var awayAtt = 0;
     var awayMid = 0;
+    
     awayTeam.players.forEach(function(player){
-       awayAtt += player.att;
-       awayMid += player.mid;
-       awayDef += player.def;
+        awayAtt += player.att;
+        awayMid += player.mid;
+        awayDef += player.def;
     });
     var awayCoeff = awayAtt + awayDef + awayMid;
     var newMatch = {};
@@ -32,6 +37,9 @@ module.exports.playMatch = function (homeTeam, awayTeam) {
         newMatch.homeGoals = Math.floor(Math.random()+3)
         newMatch.awayGoals = newMatch.homeGoals + Math.floor(Math.random()+1)
     }
+    console.log(homeCoeff);
+    console.log(awayCoeff);
+
     newMatch.homeTeam = homeTeam;
     newMatch.awayTeam = awayTeam;
     Match.create(newMatch, function(err, createdMatch){
