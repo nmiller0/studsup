6a88b96a33b5775fe3927b16ae34654a64969ce3
Neal Miller <neal.miller@outlook.com>
2019-02-28 21:39:21 -0500

    added name generations for teams

--- controllers/team.js
+++ controllers/team.js
@@ -1,15 +1,16 @@
-// name: String,
-//     city: String,
-//     sponsors: [{type: mongoose.Schema.Types.ObjectId, ref:"sponsor"}],
-//     players: [{type: mongoose.Schema.Types.ObjectId, ref:"player"}],
-//     league: mongoose.Schema.Types.ObjectId
+var faker = require("faker");
 var models = require("../models/index");
 var playerController = require("./player");
-module.exports.createTeam = async function (name, players = null, tier = null, league = null) {
+module.exports.createTeam = async function (name = null, players = null, tier = null, league = null) {
     if (!players) {
         players = await playerController.createTeamPlayers(tier);
     }
+    var city = faker.address.city();
+    if(!name){
+        name = city + " United";
+    }
     var team = {
+        city: city,
         name: name,
         players: players,
         league: league
