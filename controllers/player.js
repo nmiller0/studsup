ad163f4d33640939c9430af1800cd2aba51b8b3c
Neal Miller <neal.miller@outlook.com>
2019-04-15 10:50:23 -0400

    Improved squad/player generation

--- controllers/player.js
+++ controllers/player.js
@@ -2,24 +2,51 @@ var faker = require("faker");
 var postions = ["GK", "DEF", "MID", "ATT"];
 var models = require("../models/index");
 
-module.exports.genNewPlayer = function () {
-    var p = {};
-    p.name = faker.name.findName();
-    p.age = 22;
-    p.att = Math.random() * 5;
-    p.def = Math.random() * 5;
-    p.mid = Math.random() * 5;
-    p.position = postions[(Math.floor(Math.random() * 4))];
-    var promise = models.Player.create(p);
-    return promise;
-};
-
+var attMultipliers = {
+    att : 1.5,
+    mid : 1.0,
+    def : .6,
+    gk : .25
+}
+var midMultipliers = {
+    att : 1.0,
+    mid : 1.3,
+    def : 1.0,
+    gk : .25
+}
+var defMultipliers = {
+    att : .6,
+    mid : 1.0,
+    def : 1.3,
+    gk : .35
+}
+var gkMultipliers = {
+    att : .5,
+    mid : .5,
+    def : .75,
+    gk : 1.3
+}
+function getMultipliers(pos){
+    if(pos.toLowerCase() === "att"){
+        return attMultipliers;
+    }
+    if(pos.toLowerCase() === "mid"){
+        return midMultipliers;
+    }
+    if(pos.toLowerCase() === "def"){
+        return defMultipliers;
+    }
+    if(pos.toLowerCase() === "gk"){
+        return gkMultipliers;
+    }
+}
 module.exports.createNewPlayer = async function (name = null, position = null, age = null, tier = null) {
+    var multipliers = getMultipliers(position);
     var p = {};
-    p.att = Math.random() * tier;
-    p.def = Math.random() * tier;
-    p.mid = Math.random() * tier;
-    p.gk = Math.random() * tier;
+    p.att = (Math.random() * tier) * multipliers.att;
+    p.def = (Math.random() * tier) * multipliers.def;
+    p.mid = (Math.random() * tier) * multipliers.mid;
+    p.gk = (Math.random() * tier) * multipliers.gk;
     if (!name) {
         name = faker.name.findName();
     }
@@ -57,17 +84,24 @@ function findBestPosition(p){
     return highestPos
 }
 
-
-function createGoalkeeper(tier = null) {
-    return module.exports.createNewPlayer(null, "GK", null, tier);
-}
-
-module.exports.createTeamPlayers = async function (tier = null) {
+async function genPartialSquad(pos, num, tier){
     var players = [];
-    for (var i = 0; i < 24; i++) {
-        var newPlayer = await module.exports.createNewPlayer(null, null, null, tier);
+    for(var i = 0; i < num; i++){
+        var newPlayer = await module.exports.createNewPlayer(null, pos, null, tier);
         players.push(newPlayer);
     }
+    return players;
+}
+// This will require a lot of stuff. Probably multipliers for attributes by position? 
+// basically replacing the find best position thingy
+module.exports.createTeamPlayers = async function (tier = null) {
+    
+    var gks = await genPartialSquad("gk",3,tier);
+    var defs = await genPartialSquad("def",7,tier);
+    var mid = await genPartialSquad("mid",7,tier);
+    var att = await genPartialSquad("att",7,tier);
+
+    var players =  gks.concat(defs,mid,att);
     return players;
 };
 
